5ff84692825e55b93dbc55019775e3e1702c69b3
ggow <9898181+ggow@users.noreply.github.com>
2022-04-01 08:28:04 +0100

    config: inherit only aosp_base.mk

--- omni_suez.mk
+++ omni_suez.mk
@@ -1,7 +1,4 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-$(call inherit-product, device/amazon/suez/device.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_NAME := omni_suez
 PRODUCT_DEVICE := suez
